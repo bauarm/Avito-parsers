dd645c2eec01f58ea9f907033bf4fe4008d0a4d4
bauarm <daleemeon@gmail.com>
2019-06-25 21:44:01 +0400

    remake researcher_prof

--- tools/researcher_prof.py
+++ tools/researcher_prof.py
@@ -1,72 +1,101 @@
 import csv
 import re
 
+loader=[{'name': 'грузчик', 'key_word': 'грузч'},{'name': 'транспортировщик', 'key_word': 'транспортировщ'},{'name': 'экспедитор', 'key_word': 'экспедитор'},
+		{'name': 'наборщик', 'key_word': 'наборщ'},{'name': 'прессовщик', 'key_word': 'прессовщ'},{'name': 'приемщик', 'key_word': 'приемщ'},
+		{'name': 'комплектовщик', 'key_word': 'комплектовщ'},{'name': 'укладчик', 'key_word': 'укладч'},{'name': 'упаковщик', 'key_word': 'упаковщ'},
+		{'name': 'стикеровщик', 'key_word': 'стикеровщ'},{'name': 'сборщик', 'key_word': 'сборщ'},{'name': 'работник торгового зала', 'key_word': 'работник торгового зала'},
+		
+		{'name': 'продавец', 'key_word': 'продав'},{'name': 'кассир', 'key_word': 'кассир'},{'name': 'продавец-консультант', 'key_word': 'консультант'},
+		{'name': 'Водитель', 'key_word': 'водит'},{'name': 'таксист', 'key_word': 'таксист'},{'name': 'дальнобойшик', 'key_word': 'дальноб'},
+		{'name': 'Разнорабочий', 'key_word': 'разнораб'},{'name': 'Подсобник', 'key_word': 'подсоб'},{'name': 'Охранник', 'key_word': 'охран'}
+		]
 
-#otbor_taxist=['водитель','водитель яндекс','водитель uber','водитель такси','таксист'] # по нулевому индекс идет первомоночальный отбор ,бинарныйпоиск?
-otbor_taxist=['водител']
-gazelist=['водител газел','водитель фотон','водитель грузчик','водитель курьер','водитель экспедитор']
-taxist=['водитель яндекс','водитель uber','водитель такси','таксист']
-dalnoboy=['водител газел','водитель фотон','водитель грузчик','водитель курьер','водитель экспедитор']
+no_loader=['погрузч']
 
 def write_csv(data, file_name):
 	fieldnames = ['post','wage']
+	with open(file_name, 'a', newline='', encoding='utf-8-sig') as f:
+		writer = csv.DictWriter(f, fieldnames=fieldnames)
+		for note in data:
+			writer.writerow(note)
+		
+def write_csv2(data, file_name):
+	fieldnames = ['id', 'prof','count','min_wage','max_wage','average']
 	with open(file_name, 'a', newline='', encoding='utf-8-sig') as f:
 		#print(data)
 		writer = csv.DictWriter(f, fieldnames=fieldnames)
 		
 		for note in data:
-			#print(note)
-			writer.writerow(note)
-		
-
+			print(note)
+			writer.writerow(note)	
 
 def csv_dict_reader():
 	arr=[]
 	arr_wage=[]
-	with open('Tula&obl_vakant_23_6_2019__11_36.csv', mode='r', newline="", encoding='utf-8-sig') as f_obj:
+	with open('Kaluga&obl_vakant_21_6_2019__23_28.csv', mode='r', newline="", encoding='utf-8-sig') as f_obj:
 		reader = csv.DictReader(f_obj, delimiter=',')
-		
 		for line in reader:
 			#print(line)
-			profs={'post':(line['name']),
-				'wage':int((line['wage']))
+			profs={'id':(line['id']),
+					'post':(line['name']),
+					'wage':int((line['wage']))
 				}
 			arr.append(profs)
-	dol_cnt=0
-	prof='водител'
+	return arr
+	
+	
+	
+def prof_finder(vacant_key, vacant_full, vac_file_name):
+	prof=vacant_key
+	arr=csv_dict_reader()
 	wage_result=[]
 	prof_result=[]
+	pivot_result=[]
 	wage_sum=0
+	count=0
 	for dol in arr:
 		result = re.search(prof, str(dol['post']))
-			print('ssd')
-		if type(result) == re.Match:
-			if dol['wage']>100000:
-				
-				wage_result.append(dol['wage'])
-				prof_result.append(dol['name'])
-				wage_sum+=dol['wage']
-			#print(result.string,',',dol['wage'])
-			#print(result.string,' ')
-		dol_cnt+=1
-	min_wage=min(wage_result)
-	max_wage=max(wage_result)
-	averg=wage_sum//len(wage_result)
-	#print('MIN__',min_wage,',','MAX__',max_wage,',','AVERG__',averg)
-	write_csv(prof_result, 'vacancies&salaries_tula&obl_6_2019.csv')
-	#return profs
 		
- 
-#функция собирает общий пул похожих профессий по регионы, по корню слова
-#функция которая кроит общий пул на зараннее определенные специлизации
-#функция ыспомогательная находит профессии которые еще не известны приложению и заносит их в отдельлный список
-# нужен сбор айди чтобы вытягивать из базы некоторые эллементы по нему
-
-def otbor(arr, csvf):
-	sorted(csvf)
-	main_key=arr[0]
+		if type(result) == re.Match:
+			if dol['wage']>8000:
+				profs_target={'id':(dol['id']),
+				
+							'post':(dol['post']),
+							'wage':(dol['wage'])
+							}
+				prof_result.append(profs_target)
+				wage_result.append(dol['wage'])
+				count+=1
+				try:
+					wage_sum+=dol['wage']
+				except Exception as e:
+					print(e.__class__)
+	try:	
+		min_wage=min(wage_result)
+		max_wage=max(wage_result)
+		averg=wage_sum//len(wage_result)
+		#print(prof,' ',' ',count,' ','MIN__',min_wage,',','MAX__',max_wage,',','AVERG__',averg)
+		pivot_table={'id':(dol['id']),
+					'prof':(vacant_full),
+					'count':(count),
+					'min_wage':(min_wage),
+					'max_wage':(max_wage),
+					'average':(averg)
+					}
+		pivot_result.append(pivot_table)
+	except Exception as e:
+		print(e.__class__)
+	
+	
+	write_csv(prof_result, 'gruzch_moscow_obl_6_2019.csv')
+	write_csv2(pivot_result, vac_file_name+'.csv')
+	
+def main():
+	for work in loader:
+		prof_finder(work['key_word'],work['name'],loader[0]['name'])
 	
 if __name__ == "__main__":
-	csv_dict_reader()
+	main()
 	
 	
