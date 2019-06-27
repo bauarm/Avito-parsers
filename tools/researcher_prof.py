e29481d274f0cd7e041873ce9b3102d7dcd31e5f
bauarm <daleemeon@gmail.com>
2019-06-27 23:25:39 +0400

    vacancy analizator improved

--- tools/researcher_prof.py
+++ tools/researcher_prof.py
@@ -1,20 +1,28 @@
+import os
 import csv
 import re
 
-loader=[{'name': 'грузчик', 'key_word': 'грузч'},{'name': 'транспортировщик', 'key_word': 'транспортировщ'},{'name': 'экспедитор', 'key_word': 'экспедитор'},
-		{'name': 'наборщик', 'key_word': 'наборщ'},{'name': 'прессовщик', 'key_word': 'прессовщ'},{'name': 'приемщик', 'key_word': 'приемщ'},
-		{'name': 'комплектовщик', 'key_word': 'комплектовщ'},{'name': 'укладчик', 'key_word': 'укладч'},{'name': 'упаковщик', 'key_word': 'упаковщ'},
-		{'name': 'стикеровщик', 'key_word': 'стикеровщ'},{'name': 'сборщик', 'key_word': 'сборщ'},{'name': 'работник торгового зала', 'key_word': 'работник торгового зала'},
+loader=[{'name': 'Грузчик', 'key_word': 'грузч'},{'name': 'Транспортировщик', 'key_word': 'транспортировщ'},{'name': 'Экспедитор', 'key_word': 'экспедитор'},
+		{'name': 'Наборщик', 'key_word': 'наборщ'},{'name': 'Прессовщик', 'key_word': 'прессовщ'},{'name': 'Приемщик', 'key_word': 'приемщ'},
+		{'name': 'Комплектовщик', 'key_word': 'комплектовщ'},{'name': 'Укладчик', 'key_word': 'укладч'},{'name': 'Упаковщик', 'key_word': 'упаковщ'},
+		{'name': 'Стикеровщик', 'key_word': 'стикеровщ'},{'name': 'Сборщик', 'key_word': 'сборщ'},{'name': 'Работник торгового зала', 'key_word': 'работник торгового зала'},
 		
-		{'name': 'продавец', 'key_word': 'продав'},{'name': 'кассир', 'key_word': 'кассир'},{'name': 'продавец-консультант', 'key_word': 'консультант'},
-		{'name': 'Водитель', 'key_word': 'водит'},{'name': 'таксист', 'key_word': 'таксист'},{'name': 'дальнобойшик', 'key_word': 'дальноб'},
-		{'name': 'Разнорабочий', 'key_word': 'разнораб'},{'name': 'Подсобник', 'key_word': 'подсоб'},{'name': 'Охранник', 'key_word': 'охран'}
+		{'name': 'Продавец', 'key_word': 'продав'},{'name': 'Кассир', 'key_word': 'кассир'},{'name': 'Продавец-консультант', 'key_word': 'консультант'},
+		{'name': 'Водитель', 'key_word': 'водит'},{'name': 'Таксист', 'key_word': 'такс'},{'name': 'Дальнобойшик', 'key_word': 'дальноб'},
+		{'name': 'Разнорабочий', 'key_word': 'разнораб'},{'name': 'Подсобник', 'key_word': 'подсоб'},{'name': 'Охранник', 'key_word': 'охран'},
+		{'name': 'Менеджер', 'key_word': 'менедж'},{'name': 'Оператор', 'key_word': 'оператор'},{'name': 'Повар', 'key_word': 'повар'},
+		{'name': 'Администратор', 'key_word': 'администрат'},{'name': 'Рабочий', 'key_word': 'рабоч'},{'name': 'Официант', 'key_word': 'официант'},
+		
+		{'name': 'Кладовщик', 'key_word': 'кладовщ'},{'name': 'Мастер', 'key_word': 'маст'},{'name': 'Мерчендайзер', 'key_word': 'мерченд'},
+		{'name': 'Парикмахер', 'key_word': 'парикмах'},{'name': 'Уборщик', 'key_word': 'уборщ'},{'name': 'Автослесарь', 'key_word': 'автослес'}
+		
+		
 		]
 
 no_loader=['погрузч']
 
 def write_csv(data, file_name):
-	fieldnames = ['post','wage']
+	fieldnames = ['id','post','wage']
 	with open(file_name, 'a', newline='', encoding='utf-8-sig') as f:
 		writer = csv.DictWriter(f, fieldnames=fieldnames)
 		for note in data:
@@ -23,17 +31,17 @@ def write_csv(data, file_name):
 def write_csv2(data, file_name):
 	fieldnames = ['id', 'prof','count','min_wage','max_wage','average']
 	with open(file_name, 'a', newline='', encoding='utf-8-sig') as f:
-		#print(data)
+		print(data)
 		writer = csv.DictWriter(f, fieldnames=fieldnames)
 		
 		for note in data:
-			print(note)
+			#print(note)
 			writer.writerow(note)	
 
-def csv_dict_reader():
+def csv_dict_reader(data_set):
 	arr=[]
 	arr_wage=[]
-	with open('Kaluga&obl_vakant_21_6_2019__23_28.csv', mode='r', newline="", encoding='utf-8-sig') as f_obj:
+	with open(data_set, mode='r', newline="", encoding='utf-8-sig') as f_obj:
 		reader = csv.DictReader(f_obj, delimiter=',')
 		for line in reader:
 			#print(line)
@@ -46,9 +54,9 @@ def csv_dict_reader():
 	
 	
 	
-def prof_finder(vacant_key, vacant_full, vac_file_name):
+def prof_finder(vacant_key, vacant_full, vac_file_name, data_set):
 	prof=vacant_key
-	arr=csv_dict_reader()
+	arr=csv_dict_reader(data_set)
 	wage_result=[]
 	prof_result=[]
 	pivot_result=[]
@@ -59,6 +67,7 @@ def prof_finder(vacant_key, vacant_full, vac_file_name):
 		
 		if type(result) == re.Match:
 			if dol['wage']>8000:
+				#print(dol)
 				profs_target={'id':(dol['id']),
 				
 							'post':(dol['post']),
@@ -86,16 +95,23 @@ def prof_finder(vacant_key, vacant_full, vac_file_name):
 		pivot_result.append(pivot_table)
 	except Exception as e:
 		print(e.__class__)
-	
-	
+	print(prof_result)
+	print(pivot_result)
 	write_csv(prof_result, 'gruzch_moscow_obl_6_2019.csv')
 	write_csv2(pivot_result, vac_file_name+'.csv')
 	
-def main():
+def main(this_file):
 	for work in loader:
-		prof_finder(work['key_word'],work['name'],loader[0]['name'])
+		#print(len(loader))
+		#print(work)
+		prof_finder(work['key_word'],work['name'],loader[0]['name'],this_file)
 	
 if __name__ == "__main__":
-	main()
+	curDir=os.getcwd()
+	files = os.listdir(curDir+"\\data")
+	#print(files)
+	for file in files:
+		this_file=(curDir+"\\data"+"\\"+file)
+		main(this_file)
 	
 	
